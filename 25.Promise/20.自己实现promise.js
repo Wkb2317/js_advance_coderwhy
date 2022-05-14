087a1dac06abf20c0d140c1e1c0eadd05efaac8a
Wkb2317 <71696625+Wkb2317@users.noreply.github.com>
2022-05-14 16:30:06 +0800

    feat:自己实现简单then

--- 25.Promise/20.自己实现promise.js
+++ 25.Promise/20.自己实现promise.js
@@ -1,16 +1,16 @@
-const promise1 = new Promise((resolve, reject) => {})
+// const promise1 = new Promise((resolve, reject) => {})
 
-const promise2 = new Promise((resolve, reject) => {
-  resolve('aaa')
-})
+// const promise2 = new Promise((resolve, reject) => {
+//   resolve('aaa')
+// })
 
-const promise3 = new Promise((resolve, reject) => {
-  reject('err')
-})
+// const promise3 = new Promise((resolve, reject) => {
+//   reject('err')
+// })
 
-console.log(promise1)
-console.log(promise2)
-console.log(promise3)
+// console.log(promise1)
+// console.log(promise2)
+// console.log(promise3)
 
 const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
@@ -18,13 +18,59 @@ const REJECTED = 'rejected'
 
 class MyPromise {
   constructor(executor) {
-    // promise默认状态
-    this.status = 'pending'
+    this.status = 'pending' // promise默认状态
+    this.value = undefined
+    // 保存then传入的函数
+    this.onFulFilledFns = []
+    this.onRejectedFns = []
 
-    this.resolve = function () {}
+    this.resolve = value => {
+      if (this.status === PENDING) {
+        this.status = FULFILLED
+        this.value = value
+        this.onFulFilledFns.forEach(fn => {
+          fn(this.value)
+        })
+      }
+    }
 
-    this.reject = function () {}
+    this.reject = err => {
+      if (this.status === PENDING) {
+        this.status = REJECTED
+        this.value = err
+        this.onRejectedFns.forEach(fn => {
+          fn(this.value)
+        })
+      }
+    }
 
-    executor(resolve, reject)
+    // 在new proimse时会传入一个函数，接受两个参数，并立即执行
+    executor(this.resolve, this.reject)
+  }
+
+  then(onFulFilled, onRejected) {
+    if (this.status === PENDING) {
+      this.onFulFilledFns.push(onFulFilled)
+      this.onRejectedFns.push(onRejected)
+    } else if (this.status === FULFILLED) {
+      onFulFilled(this.value)
+    } else if (this.status === REJECTED) {
+      onRejected(this.value)
+    }
   }
 }
+
+const promise = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('aaa')
+  }, 1000)
+})
+
+promise.then(
+  res => {
+    console.log('success:', res)
+  },
+  err => {
+    console.log('err:', err)
+  }
+)
